c0cc04bb0959f76150bae8f7dd48444f1f87b4db
Nico Rehwaldt <nico.rehwaldt@camunda.com>
2020-04-16 20:35:55 +0200

    test(edit): verify missing namespace export
    Related to https://github.com/bpmn-io/bpmn-js/issues/1310

--- test/spec/xml/edit.js
+++ test/spec/xml/edit.js
@@ -6,6 +6,7 @@ import {
 
 import {
   fromFile as readFromFile,
+  validate,
   toXML
 } from '../../xml-helper';
 
@@ -77,4 +78,120 @@ describe('bpmn-moddle - edit', function() {
       });
     });
   });
+
+
+  describe('generate DI', function() {
+
+    function readAndGenerateDI(file, done) {
+
+      fromFile(file, function(err, definitions) {
+
+        if (err) {
+          return done(err);
+        }
+
+        var process = definitions.rootElements[0];
+
+        var flowElements = process.flowElements;
+
+        var [
+          start,
+          task,
+          flow
+        ] = flowElements;
+
+        var plane = definitions.diagrams[0].plane;
+
+        var model = definitions.$model;
+
+        var planeElements = plane.get('planeElement');
+
+        // when
+        planeElements.push(
+          model.create('bpmndi:BPMNEdge', {
+            id: 'Flow_di',
+            bpmnElement: flow,
+            waypoint: [
+              model.create('dc:Point', { x: 100, y: 100 }),
+              model.create('dc:Point', { x: 150, y: 150 })
+            ]
+          }),
+          model.create('bpmndi:BPMNShape', {
+            id: 'Start_di',
+            bpmnElement: start,
+            bounds: model.create('dc:Bounds', { x: 50, y: 50, width: 100, height: 100 })
+          }),
+          model.create('bpmndi:BPMNShape', {
+            id: 'Task_di',
+            bpmnElement: task,
+            bounds: model.create('dc:Bounds', { x: 100, y: 100, width: 100, height: 100 })
+          })
+        );
+
+        return done(null, definitions);
+      });
+    }
+
+
+    it('should auto-add wellknown', function(done) {
+
+      // given
+      readAndGenerateDI('test/fixtures/bpmn/local-ns-no-di.bpmn', function(err, definitions) {
+        if (err) {
+          return done(err);
+        }
+
+        // when
+        toXML(definitions, { format: true }, function(err, xml) {
+
+          // then
+          expect(xml).to.contain(
+            'xmlns:di="http://www.omg.org/spec/DD/20100524/DI"'
+          );
+
+          expect(xml).to.contain(
+            'xmlns:dc="http://www.omg.org/spec/DD/20100524/DC"'
+          );
+
+          return validate(err, xml, done);
+        });
+      });
+
+    });
+
+
+    it('should reuse global namespace', function(done) {
+
+      // given
+      readAndGenerateDI('test/fixtures/bpmn/local-ns-no-di.bpmn', function(err, definitions) {
+        if (err) {
+          return done(err);
+        }
+
+        // when
+        toXML(definitions, { format: true }, function(err, xml) {
+
+          // when
+          // set global namespace information
+          definitions.$attrs['xmlns:di'] = 'http://www.omg.org/spec/DD/20100524/DI';
+          definitions.$attrs['xmlns:dc'] = 'http://www.omg.org/spec/DD/20100524/DC';
+
+          // then
+          expect(xml).to.contain(
+            'xmlns:di="http://www.omg.org/spec/DD/20100524/DI"'
+          );
+
+          expect(xml).to.contain(
+            'xmlns:dc="http://www.omg.org/spec/DD/20100524/DC"'
+          );
+
+          return validate(err, xml, done);
+        });
+
+      });
+
+    });
+
+  });
+
 });
