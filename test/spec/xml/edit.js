f409d05d4aa99c5849447678569b5886dac422cd
Nico Rehwaldt <nico.rehwaldt@camunda.com>
2020-04-16 20:35:55 +0200

    test(edit): verify missing namespace export
    Related to https://github.com/bpmn-io/bpmn-js/issues/1310

--- test/spec/xml/edit.js
+++ test/spec/xml/edit.js
@@ -6,6 +6,7 @@ import {
 
 import {
   fromFile as readFromFile,
+  validate,
   toXML
 } from '../../xml-helper';
 
@@ -73,4 +74,113 @@ describe('bpmn-moddle - edit', function() {
       expect(xml).to.contain('<bpmn:dataObjectReference id="DataObjectReference_1" dataObjectRef="dataObject_2" />');
     });
   });
+
+
+  describe('generate DI', function() {
+
+    async function readAndGenerateDI(file) {
+
+      var {
+        rootElement: definitions
+      } = await fromFile(file);
+
+      var process = definitions.rootElements[0];
+
+      var flowElements = process.flowElements;
+
+      var [
+        start,
+        task,
+        flow
+      ] = flowElements;
+
+      var plane = definitions.diagrams[0].plane;
+
+      var model = definitions.$model;
+
+      var planeElements = plane.get('planeElement');
+
+      // when
+      planeElements.push(
+        model.create('bpmndi:BPMNEdge', {
+          id: 'Flow_di',
+          bpmnElement: flow,
+          waypoint: [
+            model.create('dc:Point', { x: 100, y: 100 }),
+            model.create('dc:Point', { x: 150, y: 150 })
+          ]
+        }),
+        model.create('bpmndi:BPMNShape', {
+          id: 'Start_di',
+          bpmnElement: start,
+          bounds: model.create('dc:Bounds', { x: 50, y: 50, width: 100, height: 100 })
+        }),
+        model.create('bpmndi:BPMNShape', {
+          id: 'Task_di',
+          bpmnElement: task,
+          bounds: model.create('dc:Bounds', { x: 100, y: 100, width: 100, height: 100 })
+        })
+      );
+
+      return {
+        definitions
+      };
+    }
+
+
+    it('should auto-add wellknown', async function() {
+
+      // given
+      var {
+        definitions
+      } = await readAndGenerateDI('test/fixtures/bpmn/local-ns-no-di.bpmn');
+
+      // when
+      var {
+        xml
+      } = await toXML(definitions, { format: true });
+
+      // then
+      expect(xml).to.contain(
+        'xmlns:di="http://www.omg.org/spec/DD/20100524/DI"'
+      );
+
+      expect(xml).to.contain(
+        'xmlns:dc="http://www.omg.org/spec/DD/20100524/DC"'
+      );
+
+      await validate(xml);
+    });
+
+
+    it('should reuse global namespace', async function() {
+
+      // given
+      var {
+        definitions
+      } = await readAndGenerateDI('test/fixtures/bpmn/local-ns-no-di.bpmn');
+
+      // when
+      // set global namespace information
+      definitions.$attrs['xmlns:di'] = 'http://www.omg.org/spec/DD/20100524/DI';
+      definitions.$attrs['xmlns:dc'] = 'http://www.omg.org/spec/DD/20100524/DC';
+
+      var {
+        xml
+      } = await toXML(definitions, { format: true });
+
+      // then
+      expect(xml).to.contain(
+        'xmlns:di="http://www.omg.org/spec/DD/20100524/DI"'
+      );
+
+      expect(xml).to.contain(
+        'xmlns:dc="http://www.omg.org/spec/DD/20100524/DC"'
+      );
+
+      await validate(xml);
+    });
+
+  });
+
 });
