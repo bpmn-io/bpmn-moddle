4f53d936d547b424f331d6a376053075b567f5ce
Nico Rehwaldt <nico.rehwaldt@camunda.com>
2022-08-19 09:34:18 +0200

    chore: disable bundle warning
    We're fine with ESM / CommonJS non-interoperability.

--- rollup.config.js
+++ rollup.config.js
@@ -50,8 +50,8 @@ export default [
   {
     input: srcEntry,
     output: [
-      { file: pkg.main, format: 'cjs' },
-      { file: pkg.module, format: 'es' }
+      { file: pkg.main, format: 'cjs', exports: 'default' },
+      { file: pkg.module, format: 'es', exports: 'default' }
     ],
     external: [
       'min-dash',
