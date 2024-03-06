87be9d3b2b47afb36ef8434a635201946f774987
Nico Rehwaldt <nico.rehwaldt@camunda.com>
2024-03-06 14:41:45 +0100

    chore: drop UMD distribution
    BREAKING CHANGE: Use ESM bundle in modern browsers

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,4 @@
-import terser from '@rollup/plugin-terser';
 import resolve from '@rollup/plugin-node-resolve';
-import commonjs from '@rollup/plugin-commonjs';
 import json from '@rollup/plugin-json';
 
 import pkg from './package.json' assert { type: "json" };
@@ -14,38 +12,7 @@ function pgl(plugins = []) {
 
 const srcEntry = pkg.source;
 
-const umdDist = pkg['umd:main'];
-
-const umdName = 'BpmnModdle';
-
 export default [
-
-  // browser-friendly UMD build
-  {
-    input: srcEntry,
-    output: {
-      file: umdDist.replace(/\.js$/, '.prod.js'),
-      format: 'umd',
-      name: umdName
-    },
-    plugins: pgl([
-      resolve(),
-      commonjs(),
-      terser()
-    ])
-  },
-  {
-    input: srcEntry,
-    output: {
-      file: umdDist,
-      format: 'umd',
-      name: umdName
-    },
-    plugins: pgl([
-      resolve(),
-      commonjs()
-    ])
-  },
   {
     input: srcEntry,
     output: [
