ec76ac22891039084935fb544a1959389c227fc2
Philipp Fromme <philipp.fromme@camunda.com>
2025-08-19 11:55:41 +0200

    fix: remove cancelRemainingInstances default value

--- test/spec/xml/roundtrip.js
+++ test/spec/xml/roundtrip.js
@@ -536,10 +536,14 @@ describe('bpmn-moddle - roundtrip', function() {
         rootElement
       } = await fromFile('test/fixtures/bpmn/ad-hoc.bpmn');
 
+      // cancelRemainingInstances should not have a default value
+      expect(rootElement.get('cancelRemainingInstances')).not.to.exist;
+
       // when
       var {
         xml
       } = await toXML(rootElement, { format: true });
+
       await validate(xml);
     });
 
