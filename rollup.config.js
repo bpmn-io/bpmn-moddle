5b8092fa08c187471bae4d323ee204bb7d31e4c6
Nico Rehwaldt <nico.rehwaldt@camunda.com>
2026-01-15 22:14:16 +0100

    chore!: drop CJS distribution
    Turns this project into an ESM only module.
    
    Requires Node >= 20.12 to use in CJS context.

--- rollup.config.js
+++ rollup.config.js
@@ -3,8 +3,6 @@ import json from '@rollup/plugin-json';
 
 import pkg from './package.json';
 
-const pkgExports = pkg.exports['.'];
-
 const srcEntry = 'lib/index.js';
 
 function pgl(plugins = []) {
@@ -19,8 +17,7 @@ export default [
   {
     input: srcEntry,
     output: [
-      { file: pkgExports.require, format: 'cjs', sourcemap: true },
-      { file: pkgExports.import, format: 'es', sourcemap: true }
+      { file: pkg.exports['.'], format: 'es', sourcemap: true }
     ],
     external: [
       'min-dash',
