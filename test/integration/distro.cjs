eb32eff37b522fe90139fa85a94db6c4e8c80e55
Nico Rehwaldt <nico.rehwaldt@camunda.com>
2026-01-15 22:20:42 +0100

    test: verify ESM module exposed

--- test/integration/distro.cjs
+++ test/integration/distro.cjs
@@ -13,6 +13,15 @@ describe('bpmn-moddle - integration', function() {
       expect(new BpmnModdle()).to.exist;
     });
 
+
+    it('should expose ESM bundle', async function() {
+      const {
+        BpmnModdle
+      } = await import('bpmn-moddle');
+
+      expect(new BpmnModdle()).to.exist;
+    });
+
   });
 
 });
